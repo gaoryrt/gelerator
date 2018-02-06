2c5a03eced193225a8fe7b00a9f7fc0c13de0a4d
日天 <gaoryrt@users.noreply.github.com>
2018-02-06 12:06:13 +0800

    🐛 uploaded the wrong branch

--- example/index.js
+++ example/index.js
@@ -2,21 +2,17 @@ const g = require('../gelerator')
 
 const $ = elemStr => document.querySelector(elemStr)
 
-const state = {
-  count: 0
-}
-
-const action = {
-  down: val => state => ({ count: state.count - val }),
-  up: val => state => ({ count: state.count + val })
-}
-
-const content = g({}, 'main')(
-  g({}, 'h1')(state.count),
-  g('down', 'button')('-'),
-  g('up', 'button')('+')
-)
-
 $('button').onclick = () => {
+  const H1 = g('bold', 'h1')('h1 with class: blod')
+  const H2 = g({ style: 'background: #ccc; color: #444' }, 'h2')('h2 with style')
+  const P = g({}, 'p')
+
+  const content = g({ id: 'content' })(
+      H1,
+      H2,
+      P('Lorem ipsum dolor sit amet'),
+      P('Consectetur adipisicing elit. Facilis, aliquid!')
+  )
+
   $('.target').appendChild(content)
 }
