4b986d9fa3147243f938e9440fbdc21a84195d72
日天 <gaoryrt@users.noreply.github.com>
2018-05-21 18:41:21 +0800

    🎨 update from mvvm branch

--- src/gelerator.js
+++ src/gelerator.js
@@ -6,12 +6,23 @@ export const g = (attrArg = {}, tagArg = 'div') => (...cttArr) => {
   let el = document.createElement(tagArg)
   let attrObj = isobject(attrArg) ? attrArg : { class: attrArg }
   Object.keys(attrObj).forEach(key => {
-    el.setAttribute(key, attrObj[key])
+    if (/^_/.test(key)) {
+      _data[attrObj[key]] = undefined
+      _watcher.on(attrObj[key], val => {
+        el[key.slice(1)] = val
+      })
+    } else if (/^\$/.test(key)) {
+      el.addEventListener(key.slice(1), e => {
+        attrObj[key](_data, e)
+      })
+    } else {
+      el.setAttribute(key, attrObj[key])
+    }
   })
   cttArr.forEach(cttItem => {
-    if (iselement(cttItem)) el.appendChild(cttItem);
-    else if (tagArg.toLowerCase() === 'img' && isstring(cttItem)) el.setAttribute('src', cttItem);
-    else el.innerText = cttItem;
+    if (iselement(cttItem)) el.appendChild(cttItem)
+    else if (tagArg.toLowerCase() === 'img' && isstring(cttItem)) el.setAttribute('src', cttItem)
+    else el.innerText = cttItem
   })
   return el
 }
