90357afeca99c728a6161c760a11568a4f5fa40c
日天 <gaoryrt@users.noreply.github.com>
2018-05-21 18:35:32 +0800

    🚸 export g instead of default

--- src/gelerator.js
+++ src/gelerator.js
@@ -2,8 +2,7 @@ import iselement from 'lodash.iselement'
 import isobject from 'lodash.isobject'
 import isstring from 'lodash.isstring'
 
-
-export default (attrArg = {}, tagArg = 'div') => (...cttArr) => {
+export const g = (attrArg = {}, tagArg = 'div') => (...cttArr) => {
   let el = document.createElement(tagArg)
   let attrObj = isobject(attrArg) ? attrArg : { class: attrArg }
   Object.keys(attrObj).forEach(key => {
