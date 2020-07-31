cfaff733a704beb8197b37b1a0b8a5d8d77db5aa
gaoryrt <gaoryrt@gmail.com>
2020-07-31 09:53:40 +0800

    ✨ mvvm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
 const iselement = el => el instanceof HTMLElement && el.nodeType === 1
 const isobject = ob => ob !== null && typeof ob === 'object'
 const isstring = st => typeof st === 'string' || st instanceof String
+const isfn = fn => typeof fn === 'function'
+const isnum = num => !isNaN(+num)
+const isarr = arr => Object.prototype.toString.call(arr) === '[object Array]'
 
-export const g = (attrArg = {}, tagArg = 'div') => (...cttArr) => {
-  let el = document.createElement(tagArg)
-  let attrObj = isobject(attrArg) ? attrArg : { class: attrArg }
-  Object.keys(attrObj).forEach(key => {
-    const val = attrObj[key]
-    if (!val) return
-    if (/^\$/.test(key)) el.setAttribute('data-' + key.slice(1), val)
-    else if (/^_/.test(key)) el.addEventListener(key.slice(1), val)
-    else if (key === 'style' && isobject(val)) {
-      el.setAttribute('style', Object.keys(val).map(i => `${i}:${val[i]}`).join(';'))
-    } else el.setAttribute(key, val)
+const ref = {}
+const watch = {}
+let init
+
+function setVal(el, val, gen, tag) {
+  // console.log(val)
+  if (tag.toLowerCase() === 'img' && isstring(val)) el.setAttribute('src', val)
+  else if (tag.toLowerCase() === 'input' && isstring(val)) el.value = val
+  else if (isfn(gen)) {
+    while (el.firstChild) el.removeChild(el.lastChild)
+    const res = gen()
+    if (isarr(res))
+      res.forEach(item => {
+        if (item) el.appendChild(item)
+      })
+    else setVal(el, res, res, tag)
+  }
+  else if (val !== undefined && val !== false) el.innerHTML = val
+}
+
+let tempSelector
+export const g = (selector = '', opt = {}) => {
+  tempSelector = selector
+  const gen = (...cttArr) => {
+    const attr = isfn(opt) ? { _click: opt } : opt
+    const matchTag = selector.match(/^\w[\w\d-_]*/)
+    const tag = matchTag ? matchTag[0] : 'div'
+    const el = document.createElement(tag)
+    const matchId = selector.match(/#\w[\w\d-_]*/)
+    const matchClass = selector.match(/\.\w[\w\d-_]*/g)
+    if (matchId) attr.id = matchId[0].slice(1)
+    if (matchClass) attr.class = matchClass.map(i => i.slice(1)).join(' ')
+    Object.keys(attr).forEach(key => {
+      const val = attr[key]
+      if (!val) return
+      if (/^_/.test(key)) el.addEventListener(key.slice(1), val)
+      else if (key === 'style' && isobject(val)) {
+        el.setAttribute('style', Object.keys(val).map(i => `${i}:${val[i]}`).join(';'))
+      } else el.setAttribute(key, val)
+    })
+    cttArr.forEach(cttItem => {
+      if (iselement(cttItem)) el.appendChild(cttItem)
+      else setVal(el, cttItem, cttItem, tag)
+    })
+    if (cttArr.length === 1) {
+      if (isnum(cttArr[0]) || isstring(cttArr[0])) ref[selector] = { el, tag }
+      else if (isfn(cttArr[0])) ref[selector] = { el, tag, gen: cttArr[0] }
+    }
+    if (selector === init) init = false
+    return el
+  }
+  if (init === undefined) init = selector
+  return gen
+}
+
+export const state = obj => {
+  const temp = { ...obj }
+  Object.keys(obj).forEach(key => {
+    Object.defineProperty(obj, key, {
+      enumerable: true,
+      get: () => {
+        if (tempSelector && init !== false) {
+          if (watch[key]) watch[key].push(tempSelector)
+          else watch[key] = [tempSelector]
+        }
+        return temp[key]
+      },
+      set: val => {
+        temp[key] = val
+        init = false
+        watch[key].forEach(selector => {
+          if (ref[selector]) {
+            const { el, tag, gen } = ref[selector]
+            if (key === 'obj') console.log(el, val, gen, tag)
+            setVal(el, val, gen, tag)
+          }
+        })
+
+      }
+    })
   })
-  cttArr.forEach(cttItem => {
-    if (iselement(cttItem)) el.appendChild(cttItem)
-    else if (tagArg.toLowerCase() === 'img' && isstring(cttItem)) el.setAttribute('src', cttItem)
-    else if (cttItem !== undefined) el.innerHTML += cttItem
-  })
-  return el
+  return obj
 }
--- src/dev.js
+++ src/dev.js
@@ -1,21 +1,109 @@
-import { g } from './App'
+import { g, state } from './App'
 
-const append = elArr => elArr.forEach(el => document.body.appendChild(el))
+const data = state({
+  newTodo: '',
+  editedTodo: null,
+  todos: [],
+  completed: false,
+  remaining: 1,
+  visibility: 'all',
+  beforeEditCache: ''
+})
 
-const el1 = g('btn')('click me')
-const el2 = g({ style: 'color: red' })('ctt')
-const el3 = g({ style: { color: 'red' } })('ctt')
-const el4 = g({ id: 'main' }, 'botton')('content')
-const el5 = g({
-  style: {
-    height: '1em',
-    width: '1em',
-    display: 'block'
-  }
-}, 'img')('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==')
-const el6 = g({
-  _click: console.log,
-  $method: 'log'
-})('console.log')
+const filters = {
+  all: todos => todos,
+  active: todos => todos.filter(todo => !todo.completed),
+  completed: todos => todos.filter(todo => todo.completed)
+}
+const pluralize = (word, count) => {
+  return word + (count < 2 ? '' : 's')
+}
 
-append([el1, el2, el3, el4, el5, el6])
+const element = g('section.todoapp')(
+  g('header.header')(
+    g('h1')('todos'),
+    g('input.new-todo', {
+      autofocus: true,
+      autocomplete: 'off',
+      placeholder: 'What needs to be done',
+      _keyup: e => {
+        if (e.key === 'Enter') {
+          data.newTodo = e.target.value
+          var value = data.newTodo && data.newTodo.trim();
+          if (!value) return
+          data.todos = [...data.todos, { id: data.todos.length + 1, title: value, completed: false }]
+          data.newTodo = ''
+        }
+      }
+    })(data.newTodo)
+  ),
+  g('section.main')(
+    g('input#toggle-all.toggle-all', {
+      type: 'checkbox'
+    })(() => filters.active(data.todos).length === 0),
+    g('label', {
+      for: 'toggle-all'
+    })('Mark all as complete'),
+    g('ul.todo-list')(
+      () => filters[data.visibility](data.todos)
+        .map((todo, index) =>
+          g(`li.todo.todo${index}`)(
+            g('.view')(
+              g('input.toggle', {
+                type: 'checkbox',
+                _click: () => {
+                  const newTodo = [...data.todos]
+                  newTodo[index].completed = !newTodo[index].completed
+                  data.todos = newTodo
+                }
+              })(todo.completed),
+              g('label.completed')(todo.title),
+              g('button.destroy', () => { console.log('remove') })()
+            ),
+            g('input.edit', {
+              type: 'text',
+              _keyup: e => {
+                if (e.key === 'Enter') {
+                  if (!data.editedTodo) {
+                    return
+                  }
+                  data.editedTodo = null
+                  todo.title = todo.title.trim()
+                }
+              }
+            })(todo.title)
+          )
+        )
+    )
+  ),
+  g('footer.footer')(
+    g('span.todo-count')(
+      () => {
+        const len = filters.active(data.todos).length
+        return !!len && (len + pluralize(' item', len) + ' left')
+      }
+    ),
+    g('ul.filters')(
+      g('li')(
+        g('a')('All'),
+        g('a')('Active'),
+        g('a')('Completed'),
+      )
+    ),
+    g('button.clear-completed')('Clear completed')
+  )
+)
+
+
+document.body.appendChild(element)
+document.body.appendChild(g('footer.info')(
+  g('p')('Double-click to edit a todo'),
+  g('p')(
+    'Written by ',
+    g('a')('Gaoryrt')
+  ),
+  g('p')(
+    'Part of ',
+    g('a')('TodoMVC')
+  )
+))
